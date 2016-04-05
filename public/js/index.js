0bcf65e02ebb1ca67cc0f349aceb314adcf89cc1
Lucas <lllencinas@gmail.com>
2016-04-04 22:43:25 -0300

    Ahora redirecciona a todos los formularios

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+
+var mapPagesLinks = [];
+mapPagesLinks['altaInterno-a'] = {pagina: "alta-interno-div.html", titulo: "Alta Interno/extension"}
+mapPagesLinks['altaLineaAnalogica-a'] = {pagina: "alta-linea-analogica-div.html", titulo: "Alta Linea analogica"}
+mapPagesLinks['altaCallForwardings-a'] = {pagina: "alta-call-forwarding-div.html", titulo: "Alta Call Forwarding"}
+mapPagesLinks['altaHuntGroup-a'] = {pagina: "alta-hunt-group-div.html", titulo: "Alta Hunt Group"}
+mapPagesLinks['altaPickupGroup-a'] = {pagina: "alta-pickup-group-div.html", titulo: "Alta Pickup Group"}
+
+mapPagesLinks['solicitar-voice-mail-a'] = {pagina: "solicitar-voice-mail-div.html", titulo: "Solicitar VoiceMail"}
+mapPagesLinks['solicitar-aparato-a'] = {pagina: "solicitar-aparato-div.html", titulo: "Solicitar Aparato"}
+mapPagesLinks['cambio-de-discado-a'] = {pagina: "cambio-de-discado-div.html", titulo: "Cambio de Discado"}
+mapPagesLinks['reset-password-a'] = {pagina: "reset-password-div.html", titulo: "Reset Password"}
+
+mapPagesLinks['re-asignar-a'] = {pagina: "re-asignar-div.html", titulo: "Re-asignar"}
+mapPagesLinks['dar-de-baja-a'] = {pagina: "dar-de-baja-div.html", titulo: "Dar de Baja"}
+mapPagesLinks['revalidar-a'] = {pagina: "revalidar-div.html", titulo: "Revalidar"}
